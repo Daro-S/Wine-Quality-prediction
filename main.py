979f654e9536cd6ddf617c2690a7daf6f369e452
Daro <darosim.itdev@gmail.com>
2024-04-20 21:50:42 +0700

    dvc data transformation and model trainer added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from wineQualityPre import logger
 from wineQualityPre.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from wineQualityPre.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from wineQualityPre.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from wineQualityPre.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -24,3 +26,23 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Training stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+     logger.exception(e)
+     raise e
